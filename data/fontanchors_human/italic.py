36774fc53ce32fb697fdd85096823af63bed4e10
Elliott Pearl <elliottmpearl@gmail.com>
2026-03-08 11:09:01 -0400

    checkpoint

--- data/fontanchors_human/italic.py
+++ data/fontanchors_human/italic.py
@@ -3,20 +3,20 @@
 
 anchors = {
     "bases": {
-        0x0061: {  # a
+        0x0061: { # a
             2: (251, -110),  # was (174, -108). x-mid of (78,175.5) to (423.5,75.5) on right edge
         },
 
-        0x0066: {  # f
+        0x0066: { # f
             2: (30, -319),  # below anchor
         },
     },
     "marks": {
-        0x030B: {
+        0x030B: { # double acute above
             0: (-57, 714), # -52 = x-mid of bbox, with -5 offset for acute weght
         },
-        0x0330: {
-            2: (-220, -70), # no offset for italic slant. center is -220
+        0x0330: { # tilde below
+            2: (-220, -70), # x-mid of bbox
         },
     },
 }
--- data/fontanchors_human/semibold_italic.py
+++ data/fontanchors_human/semibold_italic.py
@@ -3,19 +3,19 @@
 
 anchors = {
     "bases": {
-        0x0061: {  # a
+        0x0061: { # a
             # 0: (340, 645),  # test was (379,645)
             2: (277, -110),  # x-mid of (78,132) to (475,132) on right edge
         },
-        0x0066: {  # f
+        0x0066: { # f
             2: (62, -319),  # below anchor
         },
     },
     "marks": {
         0x030B: { # double acute above
-            0: (-28, 719),  # -23 x-mid of bbox, with -5 left offset for acute weight
+            0: (-28, 719),  # -23 = x-mid of bbox, with -5 left offset for acute weight
         },
-        0x0330: {
+        0x0330: { # tilde below
             2: (-196, -70), # x-mid of bbox
         },
     },
